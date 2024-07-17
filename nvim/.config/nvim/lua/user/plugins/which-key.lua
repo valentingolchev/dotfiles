f2f5d5da13d149353a8d65ab37cda7296416e5f8
Valentin Golchev <v.golchev@gmail.com>
2024-07-17 08:09:00 +0200

    update nvim

--- nvim/.config/nvim/lua/user/plugins/which-key.lua
+++ nvim/.config/nvim/lua/user/plugins/which-key.lua
@@ -8,12 +8,21 @@ return {
     -- Document existing key chains
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>c_'] = { hidden = true },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      ['<leader>d_'] = { hidden = true },
+      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>g_'] = { hidden = true },
       ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      ['<leader>h_'] = { hidden = true },
+      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      ['<leader>r_'] = { hidden = true },
+      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>s_'] = { hidden = true },
+      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      ['<leader>w_'] = { hidden = true },
+      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      ['<leader>t_'] = { hidden = true },
     }
 
     -- visual mode

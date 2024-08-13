06cf6a339fa2875c16e7d65a2aeb668fd91ba4be
Valentin Golchev <v.golchev@gmail.com>
2024-08-13 19:38:24 +0200

    update nvim, tmux

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -29,6 +29,9 @@ return {
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
     end,
   },
 }

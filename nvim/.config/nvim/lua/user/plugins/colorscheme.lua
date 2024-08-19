044bcaa8e292f18d4dad5676c50da5064f8dab74
Valentin Golchev <v.golchev@gmail.com>
2024-08-19 20:49:09 +0200

    switch to dark theme

--- nvim/.config/nvim/lua/user/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require('catppuccin').setup {
-      flavour = 'latte', -- mocha, macchiato, frappe, latte
+      flavour = 'mocha', -- mocha, macchiato, frappe, latte
       transparent_background = true,
       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { 'italic' }, -- Change the style of comments

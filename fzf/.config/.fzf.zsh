eb19db2c0d4be0da4a5ac07f2eeb0d314142f618
Valentin Golchev <v.golchev@gmail.com>
2025-07-23 15:00:05 +0200

    switch theme to Catppuccin Frappe for all configs

--- fzf/.config/.fzf.zsh
+++ fzf/.config/.fzf.zsh
@@ -10,8 +10,14 @@ catppuccin_mocha=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --multi"
+catppuccin_frappe=" \
+--color=bg+:#414559,bg:#303446,spinner:#F2D5CF,hl:#E78284 \
+--color=fg:#C6D0F5,header:#E78284,info:#CA9EE6,pointer:#F2D5CF \
+--color=marker:#BABBF1,fg+:#C6D0F5,prompt:#CA9EE6,hl+:#E78284 \
+--color=selected-bg:#51576D \
+--color=border:#414559,label:#C6D0F5"
 
-export FZF_DEFAULT_OPTS=$catppuccin_mocha
+export FZF_DEFAULT_OPTS=$catppuccin_frappe
 
 show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
 export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"

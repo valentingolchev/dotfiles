51a1f3fb00ba1baccb8af7ea6b14d73c97b6a09d
Valentin Golchev <v.golchev@gmail.com>
2025-07-23 14:54:07 +0200

    add: kitty config + switch theme to Catppuccin-Frappe

--- nvim/.config/nvim/lua/user/config/lazy.lua
+++ nvim/.config/nvim/lua/user/config/lazy.lua
@@ -28,7 +28,7 @@ require('lazy').setup {
       },
       config = function()
         require('catppuccin').setup {
-          flavour = 'mocha', -- mocha, macchiato, frappe, latte
+          flavour = 'frappe', -- mocha, macchiato, frappe, latte
           transparent_background = true,
           styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
             comments = { 'italic' }, -- Change the style of comments
--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -35,7 +35,7 @@ map('n', '<leader>st', function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd 'J'
-  vim.api.nvim_win_set_height(0, 10)
+  vim.api.nvim_win_set_height(0, 15)
 end, { desc = 'Open [S]mall [T]erminal.' })
 
 vim.api.nvim_create_autocmd('TermOpen', {
